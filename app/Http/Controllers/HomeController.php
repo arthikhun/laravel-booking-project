c67a4375937fc8cb73c5a9ba101781cb9ec27ea6
arthikhun <arthikhun.ratt@gmail.com>
2024-02-16 02:48:09 +0700

    checked

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
 
     public function booking(Request $request)
     {
-        return 'hi';
+        // return 'hi';
         // $datetime = $request->input('data.checkInDate') . ' ' . $request->input('data.checkInTime');
         $booking = new Booking();
         $booking->cus_name = $request->input('data.cusName');
